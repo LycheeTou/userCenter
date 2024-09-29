17a995162743e247ac6a65d879450d9b09e28dc3
sui <dengzhh33@gmail.com>
2024-09-29 23:29:55 +0900

    初始化之后的后端框架再删除测试类

--- userCenter/src/main/java/com/sui/usercenter/mapper/UserMapper.java
+++ userCenter/src/main/java/com/sui/usercenter/mapper/UserMapper.java
@@ -1,7 +1,6 @@
 package com.sui.usercenter.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.sui.usercenter.model.User;
 
 public interface UserMapper extends BaseMapper<User> {
 
--- userCenter/src/test/java/com/sui/usercenter/UserCenterApplicationTests.java
+++ userCenter/src/test/java/com/sui/usercenter/UserCenterApplicationTests.java
@@ -1,26 +1,13 @@
 package com.sui.usercenter;
 
-import com.sui.usercenter.mapper.UserMapper;
-import com.sui.usercenter.model.User;
-import jakarta.annotation.Resource;
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
-
-@SpringBootTest(classes = UserCenterApplication.class)
+@SpringBootTest
 class UserCenterApplicationTests {
-
-    @Resource
-    private UserMapper userMapper;
 
     @Test
     void contextLoads() {
-        System.out.println(("----- selectAll method test ------"));
-        List<User> userList = userMapper.selectList(null); //查询所有用户
-        Assert.assertEquals(5, userList.size()); //断言查询结果数量为5
-        userList.forEach(System.out::println); //打印查询结果
     }
 
 }
